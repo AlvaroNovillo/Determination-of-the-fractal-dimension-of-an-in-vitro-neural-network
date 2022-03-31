399e82b7948a422d4596d6960e010079d0682d30
AlvaroNovillo <alvanovi@ucm.es>
2022-03-31 11:36:24 +0200

    Now you can visualize various components at once

--- Fractal_paint.m
+++ Fractal_paint.m
@@ -2,40 +2,42 @@ clear all
 %Study of the evolution of the fractal dimension per DIV
 DIVS = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,18];
 
-
-r = exp(1:0.05:9); %Study region
-for i=1:length(DIVS)
-    %Reads how many files have been recorded per DIV
-    dum=sprintf('Fractal_Cm/DIV%d_.mat',DIVS(i));
-    files=load(dum);
-    [~,~,nf]= size(files.Cm_DIV); %Number of files per DIV
-    for n = 1:nf
-        for m = 1:5
-            Cm = files.Cm_DIV(m,:,n);
-            %Filter the data
-            Cm = smoothdata(Cm,'lowess',6); 
-            [r_int,int] = new_filter(r,Cm);
-            [frac_dim(m,n),delta(m,n)] = fractalfit(r_int,Cm,int); %Computes the corr.dimension as a function of m.
+for h= 1:2 %Clusters loop
+    r = exp(1:0.05:9); %Study region
+    for i=1:length(DIVS)
+        %Reads how many files have been recorded per DIV
+        dum=sprintf('Fractal_Cm_%d/DIV%d_.mat',h,DIVS(i));
+        files=load(dum);
+        [~,~,nf]= size(files.Cm_DIV); %Number of files per DIV
+        for n = 1:nf
+            for m = 1:5
+                Cm = files.Cm_DIV(m,:,n);
+                %Filter the data
+                Cm = smoothdata(Cm,'lowess',6); 
+                [r_int,int] = new_filter(r,Cm);
+                [frac_dim(m,n),delta(m,n)] = fractalfit(r_int,Cm,int); %Computes the corr.dimension as a function of m.
+            end
+        end
+        %Mean of the fractal dimension per m
+        if nf > 1
+            frac_dim(:,nf+1) = mean(frac_dim,2);
+            delta(:,nf+1) = sqrt(mean(delta,2).^2 + (tinv(.95,nf)*sqrt(1/(nf-1)*sum(sum(delta-mean(delta,2))).^2)).^2);
+            mean_frac_dim(:,i) =  frac_dim(:,nf+1);
+            mean_delta(:,i) = delta(:,nf+1);
+        else
+            mean_frac_dim(:,i) =  frac_dim(:,nf);
+            mean_delta(:,i) = delta(:,nf);
         end
     end
-    %Mean of the fractal dimension per m
-    if nf > 1
-        frac_dim(:,nf+1) = mean(frac_dim,2);
-        delta(:,nf+1) = sqrt(mean(delta,2).^2 + (tinv(.95,nf)*sqrt(1/(nf-1)*sum(sum(delta-mean(delta,2))).^2)).^2);
-        mean_frac_dim(:,i) =  frac_dim(:,nf+1);
-        mean_delta(:,i) = delta(:,nf+1);
-    else
-        mean_frac_dim(:,i) =  frac_dim(:,nf);
-        mean_delta(:,i) = delta(:,nf);
+    figure();
+    hold all;
+    for k = 1:5
+        frac_paint(mean_frac_dim(k,:),mean_delta(k,:), DIVS)
     end
+    legend('m=1','m=2','m=3','m=4', 'm=5','Location','Best');
+    title(sprintf('Giant component %d'),h)
+    hold off;
 end
-figure();
-hold all;
-for k = 1:5
-    frac_paint(mean_frac_dim(k,:),mean_delta(k,:), DIVS)
-end
-legend('m=1','m=2','m=3','m=4', 'm=5','Location','Best');
-hold off;
 %%
 function [frac_dim,delta] = fractalfit(r_int,Cm,int)
     %Computation of the fit
