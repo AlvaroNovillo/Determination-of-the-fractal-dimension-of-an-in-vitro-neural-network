76c65b181f37901b8941e3f10025e7daba6d0701
AlvaroNovillo <alvanovi@ucm.es>
2022-04-11 16:53:40 +0200

    Fractal Dimension analysis

--- Fractal_paint.m
+++ Fractal_paint.m
@@ -2,7 +2,7 @@ clear all
 %Study of the evolution of the fractal dimension per DIV
 DIVS = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,18];
 
-for h= 1:2 %Clusters loop
+for h= 1:2 %Clusters loop: loops in giant components
     r = exp(1:0.05:9); %Study region
     for i=1:length(DIVS)
         %Reads how many files have been recorded per DIV
@@ -37,7 +37,16 @@ for h= 1:2 %Clusters loop
     legend('m=1','m=2','m=3','m=4', 'm=5','Location','Best');
     title(sprintf('Giant component %d'),h)
     hold off;
+    frac_m5(h,:) = mean_frac_dim(5,:);
+    delta_frac_m5(h,:) = mean_delta(5,:);
 end
+%%
+figure();
+hold on;
+arrayfun(@(x) errorbar(DIVS,frac_m5(x,:),delta_frac_m5(x,:),'-o'),1:2)
+xlabel('DIVs')
+ylabel('\beta') 
+legend('GCC','GCC2')
 %%
 function [frac_dim,delta] = fractalfit(r_int,Cm,int)
     %Computation of the fit
@@ -47,27 +56,18 @@ function [frac_dim,delta] = fractalfit(r_int,Cm,int)
     delta = sqrt(1/(length(r_int)-2)*(sum((log(Cm(1,int)) - mean(log(Cm(1,int)))).^2))/sum((r_int - mean(r_int)).^2));
     %Linear fit statistics
     Rscore = 1 - (S.normr/norm(log(Cm(1,int)) - mean(log(Cm(1,int)))))^2;
-%     sprintf('The Correlation Dimension is %.3f +/- %.3f, with R^2 of %.3f',P_5(1),delta,Rscore)
 
 
 end
 
 function [r_int,int] = new_filter(r,Cm)
     A = diff(Cm);
-    %get the last interval where nonzero elements are
     index = (A>= max(A)/2); %Threshold
     gt=find(index~=0);
     lower = min(gt);
     upper = max(gt);
     int = lower:upper; %Interval where the fit is performed
     r_int = log(r(int)); %Differential section of r where the fit is performed 
-%     Visualization of the alorithim
-%     figure();
-%     plot(A)
-%     hold on;
-%     plot(int,A(int),'r-o')
-%     legend('Cm differences','Fit interval')
-%     hold off;
 end
 
 function frac_paint(frac_dim,delta, DIVS)
